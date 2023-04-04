fd9e742923ed04f1614a0acdd301318b4b41e98c
esero88 <eserkaraceper88@gmail.com>
2023-04-04 23:36:10 +0300

    ipynb file makes trouble, immigrated to py file

---
+++ Automating Crypto Website API Pull Using Python/crypto.py
@@ -0,0 +1,31 @@
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import json
+
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+parameters = {
+  'start':'1',
+  'limit':'5000',
+  'convert':'USD'
+}
+headers = {
+  'Accepts': 'application/json',
+  'X-CMC_PRO_API_KEY': '05517c30-2046-4183-9ebf-d0028babb65c',
+}
+
+session = Session()
+session.headers.update(headers)
+
+try:
+  response = session.get(url, params=parameters)
+  data = json.loads(response.text)
+  print(data)
+except (ConnectionError, Timeout, TooManyRedirects) as e:
+  print(e)
+
+type(data)
+
+import pandas as pd
+pd.set_option('display.max_columns', None) # shows all columns
+
+pd.json_normalize(data['data'])
