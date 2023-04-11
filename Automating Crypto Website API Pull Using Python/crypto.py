b33863aa8c4711760a4b3027c372d76de29937b2
esero88 <eserkaraceper88@gmail.com>
2023-04-11 14:15:51 +0300

    api function has been added

--- Automating Crypto Website API Pull Using Python/crypto.py
+++ Automating Crypto Website API Pull Using Python/crypto.py
@@ -28,4 +28,45 @@ type(data)
 import pandas as pd
 pd.set_option('display.max_columns', None) # shows all columns
 
-pd.json_normalize(data['data'])
+df = pd.json_normalize(data['data'])
+df['timestamp'] = pd.to_datetime('now', utc=True)
+df
+
+def api_runner():
+    global df
+    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+    parameters = {
+      'start':'1',
+      'limit':'15',
+      'convert':'USD'
+    }
+    headers = {
+      'Accepts': 'application/json',
+      'X-CMC_PRO_API_KEY': '05517c30-2046-4183-9ebf-d0028babb65c',
+    }
+
+    session = Session()
+    session.headers.update(headers)
+
+    try:
+      response = session.get(url, params=parameters)
+      data = json.loads(response.text)
+      print(data)
+    except (ConnectionError, Timeout, TooManyRedirects) as e:
+      print(e)
+    
+    df2 = pd.json_normalize(data['data'])
+    df2['timestamp'] = pd.to_datetime('now', utc=True)
+    df = df.append(df2)
+
+import os
+from time import time
+from time import sleep
+
+for i in range(333):
+    api_runner()
+    print('API Runner completed')
+    sleep(60) # sleep for 1 minute
+exit()
+
+df
